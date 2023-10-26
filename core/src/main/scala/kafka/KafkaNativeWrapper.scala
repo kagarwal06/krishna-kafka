83bc46758849c2fc7f8e9d3561d4c7e50a36364a
Krishna Agarwal <62741600+kagarwal06@users.noreply.github.com>
2023-10-26 20:09:40 +0530

    KAFKA-15444: Use main() directly

--- core/src/main/scala/kafka/KafkaNativeWrapper.scala
+++ core/src/main/scala/kafka/KafkaNativeWrapper.scala
@@ -6,19 +6,16 @@ import kafka.utils.Logging
 object KafkaNativeWrapper extends Logging {
   def main(args: Array[String]): Unit = {
     if (args.length == 0) {
-
+      throw new RuntimeException(s"Error: No operation input provided")
     }
     val operation = args.head
     val arguments = args.tail
     operation match {
-      case "storage-tool" => StorageTool.process(arguments)
-      case "kafka" => Kafka.process(arguments)
+      case "storage-tool" => StorageTool.main(arguments)
+      case "kafka" => Kafka.main(arguments)
       case _ =>
         throw new RuntimeException(s"Unknown operation $operation. " +
           s"Please provide a valid operation: 'storage-tool' or 'kafka'.")
     }
-//    println(s"DEBUG 2 : I am the new Storage tool.")
-//    args.foreach(println)
-//    process(args)
   }
 }

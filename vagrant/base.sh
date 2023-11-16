088cbdbc5277b5105d9e391c7cd17cdf77d92537
Krishna Agarwal <62741600+kagarwal06@users.noreply.github.com>
2023-11-16 16:06:24 +0530

    Add package

--- vagrant/base.sh
+++ vagrant/base.sh
@@ -124,7 +124,7 @@ build_native_image() {
   rm /opt/kafka.tgz
 
   pushd /opt/kafka
-  apt-get install -y build-essential libz-dev zlib1g-dev
+  apt-get install -y build-essential libz-dev zlib1g-dev libstdc++6
   /opt/graalvm/bin/native-image --no-fallback \
         --allow-incomplete-classpath \
         --report-unsupported-elements-at-runtime \
